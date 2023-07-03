8642d72cf626c43b046d80ba969af906599d9bea
Hanif Omar <hanif.omarr@gmail.com>
2023-07-03 17:22:28 +0800

    Change styling

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -1,5 +1,12 @@
 import { useState } from "react";
-import { View, TextInput, StyleSheet, Button, Modal } from "react-native";
+import {
+  View,
+  TextInput,
+  StyleSheet,
+  Button,
+  Modal,
+  Image,
+} from "react-native";
 
 const GoalInput = (props) => {
   const [enteredGoalText, setenteredGoalText] = useState("");
@@ -15,6 +22,7 @@ const GoalInput = (props) => {
   return (
     <Modal visible={props.onChangeModalVisible} animationType="slide">
       <View style={styles.inputContainer}>
+        <Image style={styles.image} source={require("../assets/favicon.png")} />
         <TextInput
           style={styles.textInput}
           placeholder="Your course goal!"
@@ -37,14 +45,18 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    marginBottom: 26,
-    borderBottomWidth: 1,
-    borderBottomColor: "#cccccc",
+    backgroundColor: "#FFF9C9",
+    padding: 16,
+  },
+  image: {
+    width: 150,
+    height: 150,
+    margin: 25,
   },
   textInput: {
     borderWidth: 1,
     borderColor: "#cccccc",
-    width: "70%",
+    width: "100%",
     marginRight: 10,
     padding: 8,
   },
